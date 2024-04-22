855510beb27c3492a5010a1c27715392d56c4bfe
Sajith Rathna Sampath <70060290+sajithcode@users.noreply.github.com>
2024-04-22 12:13:32 +0530

    new read file upload

---
+++ welcome/src/App.js
@@ -0,0 +1,17 @@
+
+
+import Alertnew from './Components/Alertnew';
+import NavarNew from './Components/NavarNew';
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <>
+      <NavarNew />
+      <Alertnew />
+    </>
+  );
+}
+
+export default App;
